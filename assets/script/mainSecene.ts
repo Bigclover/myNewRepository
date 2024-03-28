bf265991fe6a8c094c99b4c553c680b40ff04906
Bigclover <yjhtgj@qq.com>
2024-03-28 17:15:36 +0800

    1

--- assets/script/mainSecene.ts
+++ assets/script/mainSecene.ts
@@ -22,6 +22,10 @@ export class mainSecene extends Component {
         //还会把本地master分支和远程master分支关联起来在以后的推送或者拉取时就可以简化命令
         //git remot -v 查看远程库
         //git remot rm origin 删除远程库
+        //常用命令 git init - 初始化仓库，git add . - 添加文件到暂存区，git commit - 将暂存区内容添加到仓库中
+        //git reset回退版本 git log 查看历史提交记录
+        //分支相关 git branch 列出你在本地的分支，git branch branchName 创建一个分支 ，git checkout 分支切换,
+        //git merge branchName 将指定分支合并到当前分支 git branch -d branchName 删除分支
     }
 }
 
