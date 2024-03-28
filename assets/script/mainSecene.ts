4fbe8e9c96955710d051de77181463f8a7aa5ac1
Bigclover <yjhtgj@qq.com>
2024-03-28 15:00:16 +0800

    some git order note

--- assets/script/mainSecene.ts
+++ assets/script/mainSecene.ts
@@ -10,5 +10,18 @@ export class mainSecene extends Component {
     update(deltaTime: number) {
         
     }
+
+    testGit(){
+        //远程git库
+        //生成 SSH Key
+        //ssh-keygen -t rsa -C "youremail@example.com"
+        //直接克隆远程库：git clone git@github.com:...
+        //or关联本地库和远程库
+        //git remote add origin git@github.com:
+        //git push -u origin master 第一次推送master分支时加上-u,Git不但会把本地master分支内容推送到远程新的master分支
+        //还会把本地master分支和远程master分支关联起来在以后的推送或者拉取时就可以简化命令
+        //git remot -v 查看远程库
+        //git remot rm origin 删除远程库
+    }
 }
 
